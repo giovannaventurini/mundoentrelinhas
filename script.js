fb9f0c43d005abc06dcdf43e9eb15f03b8d7d96c
Giovanna Venturini <giovanna.venturinia@gmail.com>
2025-10-18 11:54:08 -0300

    Bugs corrigidos

--- script.js
+++ script.js
@@ -13,3 +13,21 @@
       dropdownContent.classList.remove('show');
     }
   });
+
+  // --- CÓDIGO DO BOTÃO VOLTAR AO TOPO ---
+const backToTopButton = document.getElementById("back-to-top-btn");
+
+// Função que será executada quando o usuário rolar a página
+const scrollFunction = () => {
+    // Se o usuário rolar mais de 300 pixels para baixo
+    if (window.scrollY > 300) {
+        // Adiciona a classe 'show' para mostrar o botão
+        if(backToTopButton) backToTopButton.classList.add("show");
+    } else {
+        // Remove a classe 'show' para esconder o botão
+        if(backToTopButton) backToTopButton.classList.remove("show");
+    }
+};
+
+// Adiciona um "ouvinte" que chama a função sempre que a página for rolada
+window.addEventListener("scroll", scrollFunction);
